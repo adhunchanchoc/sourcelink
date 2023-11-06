f717040cd96156d62637b65b0d3d5ef63f29d9dd
adhunchanchoc <adhunchanchoc@gmail.com>
2023-11-06 09:01:49 +0100

    clean project

---
+++ src/main/java/com/adhunchanchoc/sourcelink/SourcelinkApplication.java
@@ -0,0 +1,13 @@
+package com.adhunchanchoc.sourcelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adhunchanchoc/sourcelink/SourcelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhunchanchoc.sourcelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
